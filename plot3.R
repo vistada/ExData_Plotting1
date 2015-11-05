6c7a2f2d205696432b5f86b6d5dc318441e2f90d
vistada <vistada@yahoo.com>
2015-11-05 14:51:33 -0800

    success commit

--- plot3.R
+++ plot3.R
@@ -16,14 +16,14 @@ dpc <- hpc[(hpc$Date=="2007-02-01") | (hpc$Date=="2007-02-02"),]
 dpc <- transform(dpc, times=as.POSIXct(paste(Date, Time)))
 
 ## Set the margin and plot
-par(mar = c(2.2,4,0,0))
+par(mar = c(3,4,1,0.3))
 plot(dpc$times,dpc$Sub_metering_1, type="l", xlab="", 
      ylab="Energy sub metering")
 lines(dpc$times,dpc$Sub_metering_2,col="red")
 lines(dpc$times,dpc$Sub_metering_3,col="blue")
 legend("topright", col=c("black","red","blue"), 
        c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"),
-       lty=c(1,2), lwd=c(1,2), cex = .33, adj = c(0,1), xjust = 1, y.intersp = 1)
+       lty=c(1,2), lwd=c(1,2), cex = .375, adj = c(-.3,1), y.intersp = .6)
 
 ## Copy the graph to a png file
 
